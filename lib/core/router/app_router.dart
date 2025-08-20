71a9e1ff416080859720a0d12c5800ffd83dc55d
sakatayu <yu.sakata@hacomono.co.jp>
2025-08-20 18:36:47 +0900

    ルーティングの作成

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -8,5 +8,20 @@ class AppRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes => <AutoRoute>[
     AutoRoute(page: HomeRoute.page, initial: true),
+    AutoRoute(page: OnboardingRoute.page),
+    AutoRoute(page: LoginRoute.page),
+    AutoRoute(page: MyEventRoute.page),
+    AutoRoute(page: RecentEventsRoute.page),
+    AutoRoute(page: MyRoute.page),
+    AutoRoute(page: ChooseEventRoute.page),
+    AutoRoute(page: EventListRoute.page),
+    AutoRoute(page: EventDetailRoute.page),
+    AutoRoute(page: CreateEventRoute.page),
+    AutoRoute(page: LincaDetailRoute.page),
+    AutoRoute(page: LincaEditRoute.page),
+    AutoRoute(page: TradedLincaListRoute.page),
+    AutoRoute(page: QrCodeReadRoute.page),
+    AutoRoute(page: CreatedEventListRoute.page),
+
   ];
 }
