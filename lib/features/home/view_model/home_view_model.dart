5ba4b4d4beba0aaea33b3f1ec0f0967bc8f9c4a0
fefe <imanity.fefe@gmail.com>
2025-08-20 12:05:10 +0900

    Initial commit

---
+++ lib/features/home/view_model/home_view_model.dart
@@ -0,0 +1,11 @@
+import 'package:fefeyo_flutter_template/features/home/data/home_state.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final StateNotifierProvider<HomeViewModel, HomeState> homeViewModelProvider =
+    StateNotifierProvider<HomeViewModel, HomeState>((Ref ref) {
+  return HomeViewModel();
+});
+
+class HomeViewModel extends StateNotifier<HomeState> {
+  HomeViewModel() : super(const HomeState());
+}
