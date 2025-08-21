5ee69c8dfeb97112d63f3995f6493255b2a83af8
sakatayu <yu.sakata@hacomono.co.jp>
2025-08-21 13:09:48 +0900

    不要なインポートの削除

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'l10n/app_localizations.dart';
 
